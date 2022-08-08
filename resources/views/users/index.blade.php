2bb698779385b9a1e87cefec1dd35287d329f291
John Doe <110480546+ez-j-doe@users.noreply.github.com>
2022-08-08 14:45:34 -0300

    RC-4 (#8)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -15,7 +15,7 @@
                 <td>{{ $user->id }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
-                <td>{{ $user->updated_at }}</td>
+                <td>{{ $user->created_at }}</td>
             </tr>
         @endforeach
         </tbody>
